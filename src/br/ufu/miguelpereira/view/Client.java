323910ac768e1cf53449aeb21b32b1aa46fffd85
Miguel dos Santos Pereira <miguelpereira.aux@outlook.com>
2017-10-02 16:18:07 -0300

    Adicionado parametrizacao das configuracoes do servidor e conexao do cliente

--- src/br/ufu/miguelpereira/view/Client.java
+++ src/br/ufu/miguelpereira/view/Client.java
@@ -9,17 +9,38 @@ import org.apache.thrift.transport.TTransport;
 import org.apache.thrift.transport.TSocket;
 import org.apache.thrift.protocol.TProtocol;
 import org.apache.thrift.protocol.TBinaryProtocol;
-import sun.misc.IOUtils;
 
 import java.io.FileInputStream;
-import java.io.ObjectInputStream;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.Scanner;
 
 public class Client {
 
-    public static void main(String [] args) {
-        try{
-            TTransport transport = new TSocket("localhost",9080);
+    public static Properties getProp() throws IOException {
+        Properties props = new Properties();
+        FileInputStream file = new FileInputStream(
+                "host.properties");
+        props.load(file);
+        return props;
+    }
+
+    public static void main(String[] args) {
+        String hostname;
+        int port;
+        Properties prop;
+        try {
+            prop = getProp();
+            if (!prop.isEmpty()) {
+                hostname = prop.getProperty("prop.server.host");
+                port = Integer.parseInt(prop.getProperty("prop.server.port"));
+            } else {
+                System.out.println("ERRO! Falha ao carregar arquivo de configuracao!");
+                return;
+            }
+
+
+            TTransport transport = new TSocket(hostname, port);
             transport.open();
 
             TProtocol protocol = new TBinaryProtocol(transport);
@@ -32,7 +53,7 @@ public class Client {
             Scanner scan = new Scanner(System.in);
 
             //Menu
-            while(menu!=16) {
+            while (menu != 16) {
                 System.out.println("1 - Inserir Vertice");
                 System.out.println("2 - Inserir Aresta");
                 System.out.println("3 - Carregar Grafo");
@@ -42,7 +63,7 @@ public class Client {
                 System.out.println("7 - Remover Aresta");
                 System.out.println("8 - Alterar Vertice");
                 System.out.println("9 - Alterar Aresta");
-           //   System.out.println("10 - Listar Vertices de uma aresta");
+                //   System.out.println("10 - Listar Vertices de uma aresta");
                 System.out.println("10 - Listar Arestas de um vertice");
                 System.out.println("11 - Listar Vértices adjacentes a outros");
                 System.out.println("12 - Sair");
@@ -60,15 +81,15 @@ public class Client {
                         descricao = scan.nextLine();
                         System.out.println("Peso <double>: ");
                         peso = scan.nextDouble();
-                        if(client.createVertex(v1, cor, descricao, peso)){
+                        if (client.createVertex(v1, cor, descricao, peso)) {
                             System.out.println("<- Vertice inserido! ->");
                             System.out.println(client.showVertex());
-                        }else{
+                        } else {
                             System.out.println("<- Erro! ->");
                         }
-                        
+
                         break;
-                        
+
                     case 2://Inserir aresta
 
                         System.out.println("<- Digite os devidos atributos da aresta ->");
@@ -83,68 +104,68 @@ public class Client {
                         descricao = scan.nextLine();
                         System.out.println("Digite o peso: ");
                         peso = scan.nextDouble();
-                        if(client.createEdge(v1, v2, peso, cor, descricao)){
+                        if (client.createEdge(v1, v2, peso, cor, descricao)) {
                             System.out.println("<- Aresta Inserida! ->");
                             System.out.println(client.showEdge());
-                        }else{
+                        } else {
                             System.out.println("<- Erro! ->");
                         }
-                        
+
                         break;
-                    
+
                     case 3://Carregar grafo
-                        
+
                         System.out.println("<- Informe o arquivo a ser carregado -> ");
                         System.out.println("Arquivo:");
                         scan.nextLine();
                         file = scan.nextLine();
                         client.loadGraph(file);
-                        
+
                         break;
-                        
+
                     case 4://Salvar grafo
-                        
+
                         System.out.println("<- Informe o arquivo para salvar ->");
                         System.out.println("Arquivo: ");
                         scan.nextLine();
                         file = scan.nextLine();
                         client.saveGraph(file);
-                        
+
                         break;
-                        
+
                     case 5://Mostrar grafo
-                    	System.out.println(client.showGraph());
+                        System.out.println(client.showGraph());
                         break;
 
                     case 6://Remover vertice
-                        
+
                         System.out.println("<- Digite o vertice que deseja remover ->");
                         System.out.println("Vertice: ");
                         v1 = scan.nextInt();
-                        if(client.deleteVertex(v1)){
+                        if (client.deleteVertex(v1)) {
                             System.out.println("<- Vértice Removido! ->");
                             System.out.println(client.showGraph());
-                        }else{
+                        } else {
                             System.out.println("<- Erro! ->");
                         }
                         break;
 
                     case 7://Remover aresta
-                        
+
                         System.out.println("<- Entre com o vertice 'X' da aresta que será removida ->");
                         v1 = scan.nextInt();
                         System.out.println("<- Entre com o vertice 'Y' ->");
                         v2 = scan.nextInt();
-                        if(client.deleteEdge(v1, v2)){
+                        if (client.deleteEdge(v1, v2)) {
                             System.out.println("<- Aresta removida! ->");
                             System.out.println(client.showEdge());
-                        }else{
+                        } else {
                             System.out.println("<- Erro! ->");
                         }
                         break;
 
                     case 8://Alterar vertice
-                        
+
                         System.out.println("<- Digite os atributos do vertice a ser alterado ->");
                         System.out.println("Número do vértice a ser alterado: ");
                         v2 = v1 = scan.nextInt();
@@ -155,16 +176,16 @@ public class Client {
                         descricao = scan.nextLine();
                         System.out.println("Digite o peso: ");
                         peso = scan.nextDouble();
-                        if(client.updateVertex(v1, new Vertex(v2, cor, descricao, peso))){
+                        if (client.updateVertex(v1, new Vertex(v2, cor, descricao, peso))) {
                             System.out.println("<- Vertice Alterado! ->");
                             System.out.println(client.showVertex());
-                        }else{
+                        } else {
                             System.out.println("<- Erro! ->");
                         }
                         break;
 
                     case 9://Alterar aresta
-                        
+
                         System.out.println("<- Digite os atributos da aresta a ser alterada ->");
                         System.out.println("Vertice X: ");
                         v1 = scan.nextInt();
@@ -177,10 +198,10 @@ public class Client {
                         peso = scan.nextDouble();
                         System.out.println("Digite o novo flag: ");
                         cor = scan.nextInt(); //cor sendo utilizado pra peso
-                        if(client.updateEdge(v1, v2, new Edge(v1, v2, peso, cor, descricao))){//cor é peso
+                        if (client.updateEdge(v1, v2, new Edge(v1, v2, peso, cor, descricao))) {//cor é peso
                             System.out.println("<- Aresta Alterada! ->");
                             System.out.println(client.showEdge());
-                        }else{
+                        } else {
                             System.out.println("<- Erro! ->");
                         }
                         break;
@@ -196,7 +217,7 @@ public class Client {
                         break;*/
 
                     case 10://Listar arestas de um vertice
-                        
+
                         System.out.println("<- Digite um vertice para listar suas arestas -> ");
                         System.out.println("Vertice X: ");
                         v1 = scan.nextInt();
@@ -204,28 +225,28 @@ public class Client {
                         break;
 
                     case 11://Listar vertice adjacentes
-                        
+
                         System.out.println("<- Digite o vertice para exibir suas adjacências -> ");
                         v1 = scan.nextInt();
                         System.out.println(client.showAdjacency(v1));
                         break;
 
                     case 12:
-                        
+
                         System.out.println("<- Disconnecting... ->");
                         return;
 
                     default:
                         System.out.println("<- Opção Inválida! ->");
                 }
-                try{
+                try {
                     Thread.sleep(2000);
-                }catch (InterruptedException e){
+                } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
             transport.close();
-        }catch (TException x){
+        } catch (Exception x) {
             x.printStackTrace();
         }
     }
--- src/br/ufu/miguelpereira/view/Server.java
+++ src/br/ufu/miguelpereira/view/Server.java
@@ -7,12 +7,35 @@ import org.apache.thrift.server.*;
 import org.apache.thrift.transport.TServerSocket;
 import org.apache.thrift.transport.TServerTransport;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class Server {
+
+    public static Properties getProp() throws IOException {
+        Properties props = new Properties();
+        FileInputStream file = new FileInputStream(
+                "host.properties");
+        props.load(file);
+        return props;
+    }
+
     public static void main(String [] args){
+        int port;
+        Properties prop;
         try{
+            prop = getProp();
+            if (!prop.isEmpty()) {
+                port = Integer.parseInt(prop.getProperty("prop.server.port"));
+            } else {
+                System.out.println("ERRO! Falha ao carregar arquivo de configuracao!");
+                return;
+            }
+
             GraphHandler handler = new GraphHandler();
             Operations.Processor processor = new Operations.Processor(handler);
-            TServerTransport serverTransport = new TServerSocket(9080);
+            TServerTransport serverTransport = new TServerSocket(port);
             TServer server = new TThreadPoolServer(new TThreadPoolServer.Args(serverTransport).processor(processor));
             //Criar concorrencia com apenas um thread
             //TNonblockingServerSocket serverTransport = new TNonblockingServerSocket(9080);
